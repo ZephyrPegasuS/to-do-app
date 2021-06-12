23dd31ffe7798ff7b1e7ccdc30fc0e875003536f
Nguyễn Minh Tuấn <tuan.nm@appfast.io>
2021-06-12 17:43:50 +0700

    HOC AddeD

---
+++ src/HOC/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import SwitchTheme from "../components/SwitchTheme";
+import {
+  EMPTY_TASK_LIST,
+  EMPTY_ACTIVE_TASK_LIST,
+  EMPTY_DONE_TASK_LIST,
+  TODO_STATUS,
+} from "../constants";
+import "./style.css";
+
+export const withTaskListEmpty = (Component, statusFilter) => (props) =>
+  !props.taskList.length ? (
+    <div className='empty'>
+      <img src='/empty.png' alt='image' height='85%' />
+      <p>
+        {statusFilter === "ALL"
+          ? EMPTY_TASK_LIST
+          : statusFilter === TODO_STATUS.ACTIVE
+          ? EMPTY_ACTIVE_TASK_LIST
+          : EMPTY_DONE_TASK_LIST}
+      </p>
+    </div>
+  ) : (
+    <Component {...props} />
+  );
+
+export const withThemeSwitcher = (Component) => (props) => (
+  <div className='display-button'>
+    <Component {...props} />
+    <SwitchTheme className='display-button__switch-theme' />
+  </div>
+);
