1bc8562d6628a5061514b9a337da3785b25a2e6c
Nguyễn Minh Tuấn <tuan.nm@appfast.io>
2021-06-12 17:44:13 +0700

    Constants File Added

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+// Notifications
+export const CONFIRM_DELETE_TASK = "Are you sure you want to delete this task?";
+export const TASK_PLACEHOLDER = "Enter task name here!";
+export const CREATED_SUCCESS = "You just created a new task!";
+export const CREATED_FAILED = "Please fill your task's content!";
+export const DELETED_SUCCESS = "This task has been deleted!";
+export const TOGGLE_ALL_SUCCESS = "All tasks status have been inverted!";
+export const EMPTY_TASK_LIST = "You have no tasks!";
+export const EMPTY_ACTIVE_TASK_LIST = "You have no active tasks!";
+export const EMPTY_DONE_TASK_LIST = "You have no done tasks!";
+
+// Tooltips
+export const ALL_TOOLTIP = "Will show all tasks when click!";
+export const ACTIVE_TOOLTIP = "Will show all active tasks when click!";
+export const DONE_TOOLTIP = "Will show all done tasks when click!";
+export const TOGGLE_ALL_TOOLTIP =
+  "Will convert all active tasks to done tasks and vice versa when click!";
+
+// Todo status
+export const TODO_STATUS = {
+  ACTIVE: "ACTIVE",
+  DONE: "DONE",
+};
